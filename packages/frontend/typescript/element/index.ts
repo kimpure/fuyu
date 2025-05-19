c21827eba0bdc7cdfb84bacf3627979afdd23bb1
kimpure <gimleni593@gmail.com>
2025-05-19 11:48:02 +0000

    style (index.ts, render.ts)

--- packages/frontend/typescript/element/index.ts
+++ packages/frontend/typescript/element/index.ts
@@ -3,6 +3,7 @@ export const renderElement = document.querySelector('.render') as HTMLDivElement
 export class RbxElement {
     element: HTMLDivElement;
     childs: RbxElement[];
+    parent?: RbxElement;
 
     constructor() {
         this.element = document.createElement('div');
