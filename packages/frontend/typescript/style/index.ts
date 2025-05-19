ceb6db66ce8ee4dd156ac589129ef66507849ddc
kimpure <gimleni593@gmail.com>
2025-05-19 11:48:14 +0000

    style (index.ts, render.ts)

---
+++ packages/frontend/typescript/style/index.ts
@@ -0,0 +1,18 @@
+import { initRenderStyle } from "./render.js";
+
+export function initStyle() {
+    const html = document.querySelector('html') as HTMLElement;
+    const body = document.querySelector('body') as HTMLBodyElement;
+
+    html.style.height = '100%';
+    body.style.height = '100%';
+    html.style.width = '100%';
+    body.style.width = '100%';
+
+    html.style.margin = '0';
+    body.style.margin = '0';
+    html.style.padding = '0';
+    body.style.padding = '0';
+
+    initRenderStyle();
+};
---
+++ packages/frontend/typescript/style/render.ts
@@ -0,0 +1,7 @@
+export function initRenderStyle() {
+    const render = document.querySelector('.render') as HTMLDivElement;
+    render.style.height = '100%';
+    render.style.width = '100%';
+    render.style.margin = '0';
+    render.style.padding = '0';
+};
